b29679d992d79e9383882ac0e53568cdb9a2a49b
Adam Žižka <147625102+ZizArmy@users.noreply.github.com>
2025-05-18 13:54:26 +0200

    Updates

---
+++ assets/swiper-CPfNAmpK.js
@@ -0,0 +1,1 @@
+import e from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";new e(".mySwiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},slidesPerView:1,centeredSlides:!1,spaceBetween:0});
--- assets/swiper-CbdLey0o.js
+++
@@ -1,1 +0,0 @@
-import e from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";new e(".mySwiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
